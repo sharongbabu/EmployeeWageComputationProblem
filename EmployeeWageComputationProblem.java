31784f304023172b5e8d08e47709ea66ffac6da3
“Enter <merin@gmail.com>
2020-11-01 09:44:27 +0530

    Employee Part time wage

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -52,6 +52,37 @@ System.out.println("Employee is absent");
 
 
 
+// Part time employee
+import java.util.*;
+import java.util.Random;
+public class PartTime{
+public static void main(String args[]){
+int min =0;
+int max=1;
+int random_int = (int)(Math.random() * (max - min + 1) + min);
+System.out.println(random_int);
+if ( random_int > 0 ){
+System.out.println("Employee is full day employee");
+int WagePerHr=20;
+int FullDayHr=10;
+int salary;
+salary=  WagePerHr *  FullDayHr;
+System.out.println(salary);
+}
+else
+{
+int WagePerHr=20;
+int PartDayHr=8;
+int salary;
+salary=  WagePerHr *  PartDayHr;
+System.out.println(salary);
+
+System.out.println("Employee is parttime employee");
+}
+}
+}
+
+
 
 
 
